9886a1f6c0ed9e306156113c784082acdc917784
bangms <uubang@knou.ac.kr>
2020-07-09 19:40:36 +0900

    react 반복문(map) 객체형식사용 / key

--- react-webgame/numberbaseball/NumberBaseball.jsx
+++ react-webgame/numberbaseball/NumberBaseball.jsx
@@ -32,37 +32,21 @@ class NumberBaseball extends Component {
         <ul>
           {[
             { fruit: '사과', taste: '맛있다'},
-            ['바나나', '맛있다'],
-            ['포도', '맛있다'],
-            ['귤', '맛있다'],
-            ['감', '맛있다'],
-            ['배', '맛있다'],
-            ['복숭아', '맛있다'],
-          ].map((v)=>{ // map을 반복문으로 사용
-            return (
-            <li><b> {v[0]} </b> - {v[1]} </li>
-            )
-          })}
-          {/* <li><b>사과</b> - 맛있다</li>
-          <li><b>바나나</b> - 맛있다</li>
-          <li><b>포도</b> - 맛있다</li>
-          <li><b>귤</b> - 맛있다</li>
-          <li><b>감</b> - 맛있다</li>
-          <li><b>배</b> - 맛있다</li>
-          <li><b>복숭아</b> - 맛있다</li> */}
+            { fruit: '바나나', taste: '맛없다'},
+            { fruit: '포도', taste: '시다'},
+            { fruit: '귤', taste: '쓰다'},
+            { fruit: '감', taste: '떫다'},
+            { fruit: '배', taste: '달다'},
+            { fruit: '복숭아', taste: '딱딱하다'},
+          ].map( (v, i) => // map을 반복문으로 사용 // 화살표 함수에서는 리턴이 없어도 가능 // return 대신 ()만 사용 가능
+            <li key={i}><b> {v.fruit} </b> - {v.taste} </li> // key는 고유한 값으로 // key에 index 값을 넣으면 안됨 // 나중에 성능 최적화 할 때 문제가 생김 // 요소가 추가만 되는 배열인 경우 i를 써도 되긴 함 (삭제 X) 
+            // react에서 key를 기준으로 엘리먼트를 추가하거나 수정 삭제를 판단하기 때문에 배열의 순서가 바뀌면 문제가 생김
+          )}
         </ul>
       </>
     );
   }
 }
 
-// export const hello = 'hello'; // import { hello };
-// export const bye = 'hello'; // import { hello, bye };
+export default NumberBaseball; 
 
-export default NumberBaseball; // import NumberBaseball;
-// default는 한번만
-// webpack에 들어있는 babel이 알아서 import를 바꿔줌
-// Node 에서는 밑에만 지원
-// const React = require('react');
-// exports.hello = 'hello';
-// module.exports = NumberBaseball;
