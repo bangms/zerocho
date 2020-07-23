a7bff9e6c4b2c3669381aaacb06f19ff8a16f3a1
bangms <uubang@knou.ac.kr>
2020-07-23 22:01:37 +0900

    숫자야구 Hooks 전환

--- react-webgame/numberbaseball/NumberBaseball.jsx
+++ react-webgame/numberbaseball/NumberBaseball.jsx
@@ -1,6 +1,4 @@
-const React = require('react');
-const { Component } = React;
-
+import React, { useState } from 'react';
 import Try from './Try';
 
 function getNumbers() { // 숫자 네개를 겹치지 않고 랜덤하게 뽑는 함수
@@ -8,94 +6,80 @@ function getNumbers() { // 숫자 네개를 겹치지 않고 랜덤하게 뽑는
   const array = [];
 
   for (let i = 0; i < 4; i++) {
-
     const chosen = candidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
     array.push(chosen);
   }
   return array;
 }
 
-class NumberBaseball extends Component {
-  state = {
-    result: '',
-    value: '',
-    answer: getNumbers(), // ex : [1,3,5,7]
-    tries: [],
-  };
+const NumberBaseball = () => {
+  const [result, setResult] = useState('');
+  const [value, setValue] = useState('');
+  const [answer, setAnswer] = useState(getNumbers());
+  const [tries, setTries] = useState([]);
 
-  onSubmitForm = (e) => {
+  const onSubmitForm = (e) => {
+    const { value, tries, answer } = this.state;
     e.preventDefault();
-    if (this.state.value === this.state.answer.join('')) {
-      this.setState({
-        result: '홈런!',
-        tries: [...this.state.tries, {try: this.state.value, result: '홈런!' }], // 옛날꺼 복사해놓고 [...] 새로운거 넣어주는 방법으로
-      })
+    if (value === answer.join('')) {
+      setResult('홈런!');
+      setTries((prevTries) => {
+        return [...prevState.tries, {try: tvalue, result: '홈런!' }]
+      });
+      alert('게임을 다시 시작합니다!');
+      setValue('');
+      setAnswer(getNumbers());
+      setTries([]);
     } else {
-      const answerArray = this.state.value.split('').map( (v) => parseInt(v));
+      const answerArray = value.split('').map( (v) => parseInt(v));
       let strike = 0;
       let ball = 0;
-      if (this.state.tries.length >= 9) {
-        this.setState({
-          result: `10번 넘게 틀려서 실패! 답은 ${answer.join(',')} 였습니다!`,
-        });
+      if (tries.length >= 9) {
+        setResult(`10번 넘게 틀려서 실패! 답은 ${answer.join(',')} 였습니다!`);
         alert('게임을 다시 시작합니다!');
-        this.setState({
-          value: '',
-          answer: getNumbers(),
-          tries: [],
-        });
+        setValue('');
+        setAnswer(getNumbers());
+        setTries([]);
       } else {
         for (let i = 0; i < 4; i++) {
-          if (answerArray[i] === this.state.answer[i]) { 
+          if (answerArray[i] === answer[i]) { 
             strike += 1;
-          } else if (this.state.answer.includes(answerArray[i])) {
+          } else if (answer.includes(answerArray[i])) {
             ball += 1;
           }
         }
-        this.setState({
-          tries: [...this.state.tries, {try: this.state.value, result: `${strike} 스트라이크, ${ball} 볼입니다`}],
-          value: '',
+        setTries((prevTries) => {
+          return [...prevState.tries, {try: value, result: `${strike} 스트라이크, ${ball} 볼입니다`}]
         });
+        setValue('');
       }
     }
   };
 
-  onChangeInput = (e) => {
-    console.log(this.state.answer);
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
     this.setState({
-      value: e.target.value,
+      value: value,
     })
   };
 
-  fruits = [
-    { fruit: '사과', taste: '맛있다'},
-    { fruit: '바나나', taste: '맛없다'},
-    { fruit: '포도', taste: '시다'},
-    { fruit: '귤', taste: '쓰다'},
-    { fruit: '감', taste: '떫다'},
-    { fruit: '배', taste: '달다'},
-    { fruit: '복숭아', taste: '딱딱하다'},
-  ]
-
-  render() {
-    return (
-      <>
-        <h1>{this.state.result}</h1>
-        <form onSubmit={this.onSubmitForm}>
-          {/* jsx에서 주석을 남기는 방법 */}
-          <input maxLength={4} value={this.state.value} onChange={this.onChangeInput} />
-        </form>
-        <div>시도 : {this.state.tries.length}</div>
-        <ul>
-          {this.state.tries.map( (v, i) => {
-            return (
-              <Try key={`${i}차 시도 : `} tryInfo={v} />
-            )
-          })}
-        </ul>
-      </>
-    );
-  }
+  return (
+    <>
+      <h1>{result}</h1>
+      <form onSubmit={this.onSubmitForm}>
+        {/* jsx에서 주석을 남기는 방법 */}
+        <input maxLength={4} value={value} onChange={this.onChangeInput} />
+      </form>
+      <div>시도 : {tries.length}</div>
+      <ul>
+        {tries.map( (v, i) => {
+          return (
+            <Try key={`${i}차 시도 : `} tryInfo={v} />
+          )
+        })}
+      </ul>
+    </>
+  )
 }
 
 export default NumberBaseball; 
